849957f02234ee9ce715b67ca386678ef3832ffb
mllmale <leticiacle2012@hotmail.com>
2025-04-24 20:50:09 -0300

    adicionado entity do usuario

---
+++ src/main/java/com/collab_space_api/entity/UserEntity.java
@@ -0,0 +1,38 @@
+package com.collab_space_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password_hash; // Hashed password
+
+    @Column(nullable = false)
+    private String provider; // "google", "github", etc.
+
+    @Column(nullable = false)
+    private String providerId; // ID from the OAuth provider
+
+    @Column(nullable = false)
+    private String role; // "user", "admin", etc.
+
+    @Column(nullable = false)
+    private String createdAt; // Timestamp of when the user was created
+
+    public UserEntity() {
+
+    }
+}
