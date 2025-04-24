7c043dd429e0443d6c125169db93201c9b45ff40
mllmale <leticiacle2012@hotmail.com>
2025-04-24 20:57:06 -0300

    adicionado entity do team

--- src/main/java/com/collab_space_api/entity/TeamEntity.java
+++ src/main/java/com/collab_space_api/entity/TeamEntity.java
@@ -2,6 +2,7 @@ package com.collab_space_api.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.*;
 
@@ -15,4 +16,9 @@ import lombok.*;
 @ToString
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class TeamEntity {
+    @Id
+    private String id;
+    private String name;
+    private String owner_id; // User ID of the owner
+    private String crateAt; // Timestamp of when the team was created
 }
