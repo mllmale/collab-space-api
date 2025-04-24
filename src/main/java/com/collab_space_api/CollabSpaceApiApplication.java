b4d0639f3ccf9d5c29db5563d20dc829d68c7d59
mllmale <leticiacle2012@hotmail.com>
2025-04-23 22:08:33 -0300

    inicializando dependencias spring

---
+++ src/main/java/com/collab_space_api/CollabSpaceApiApplication.java
@@ -0,0 +1,13 @@
+package com.collab_space_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabSpaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabSpaceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collab_space_api/CollabSpaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collab_space_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabSpaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
