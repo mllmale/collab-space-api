a9f4a83f10a0f169a7f8e6c85a7a9191f05648d4
mllmale <leticiacle2012@hotmail.com>
2025-04-25 00:17:16 -0300

    repository notification

---
+++ src/main/java/com/collab_space_api/repository/NotificationRepository.java
@@ -0,0 +1,17 @@
+package com.collab_space_api.repository;
+
+import com.collab_space_api.entity.NotificationEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository {
+    List<NotificationEntity> findByUserId(String userId);
+    List<NotificationEntity> findByRead(boolean read);
+    List<NotificationEntity> findByCreatedAt(String createdAt);
+    List<NotificationEntity> findByUserIdAndRead(String userId, boolean read);
+    List<NotificationEntity> findByUserIdAndCreatedAt(String userId, String createdAt);
+    List<NotificationEntity> findByReadAndCreatedAt(boolean read, String createdAt);
+
+}
