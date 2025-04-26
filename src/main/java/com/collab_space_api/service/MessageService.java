eb0311e30699e00fa9393c96ddabddd411465678
mllmale <leticiacle2012@hotmail.com>
2025-04-25 23:54:33 -0300

    adcionando decorador DATA e criado service message

--- src/main/java/com/collab_space_api/service/MessageService.java
+++ src/main/java/com/collab_space_api/service/MessageService.java
@@ -29,4 +29,47 @@ public class MessageService {
      public List<MessageEntity> getByUserId(String senderId) {
          return messageRepository.findBySenderId(senderId);
      }
+    // READ - buscar todas de um projeto
+    public List<MessageEntity> getByProjectId(String projectId) {
+        return messageRepository.findByProjectId(projectId);
+    }
+    // READ - buscar todas de um time
+    public List<MessageEntity> getByTeamId(String teamId) {
+        return messageRepository.findByTeamId(teamId);
+    }
+
+     // UPDATE
+     public MessageEntity updateMessage(MessageEntity message) {
+         return messageRepository.save(message);
+     }
+
+     // DELETE
+     public void deleteById(String id) {
+         messageRepository.deleteById(id);
+     }
+
+     public List<MessageEntity> getBySenderIdAndProjectId(String senderId, String projectId) {
+         return messageRepository.findBySenderIdAndProjectId(senderId, projectId);
+     }
+
+     public List<MessageEntity> getBySenderIdAndTeamId(String senderId, String teamId) {
+         return messageRepository.findBySenderIdAndTeamId(senderId, teamId);
+     }
+
+     public List<MessageEntity> getBySenderIdAndContent(String senderId, String content) {
+         return messageRepository.findBySenderIdAndContent(senderId, content);
+     }
+
+     public List<MessageEntity> getBySenderIdAndSentAt(String senderId, String sentAt) {
+         return messageRepository.findBySenderIdAndSentAt(senderId, sentAt);
+     }
+
+     public List<MessageEntity> getByProjectIdAndTeamId(String projectId, String teamId) {
+         return messageRepository.findByProjectIdAndTeamId(projectId, teamId);
+     }
+
+     public List<MessageEntity> getByProjectIdAndContent(String projectId, String content) {
+         return messageRepository.findByProjectIdAndContent(projectId, content);
+     }
+
 }
